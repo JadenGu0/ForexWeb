7e5f50a6c232de97d33bb8d14382188ee7bcf419
jaden <gxy199172@126.com>
2018-08-20 12:33:08 +0800

    modify bugs

--- ForexWeb/settings.py
+++ ForexWeb/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'ForexWeb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Forex',
+        'USER':'root',
+        'PASSWORD':'gxy199172',
+        'HOST':'localhost',
         }
 }
 
